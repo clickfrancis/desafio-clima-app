e0b86ac1fd182804fea0997a47516f8a5263884e
Francis Santos <francis.source@gmail.com>
2024-07-22 15:07:43 -0300

    estilizando o radio group radio form

--- src/components/registerForm/form/radioForm/index.tsx
+++ src/components/registerForm/form/radioForm/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Flex, Radio } from 'antd';
+import './style.scss'
 
 const RadioForm: React.FC = () => (
   <Flex vertical gap="middle">
-    <Radio.Group defaultValue="a" buttonStyle="solid">
-      <Radio.Button value="a">Hangzhou</Radio.Button>
-      <Radio.Button value="b">Shanghai</Radio.Button>
-      <Radio.Button value="c">Beijing</Radio.Button>
-      <Radio.Button value="d">Chengdu</Radio.Button>
+    <Radio.Group defaultValue="" buttonStyle="solid" className='custom-radio-group' >
+      <Radio.Button value="a" className='custom-radio'>MANHÃ</Radio.Button>
+      <Radio.Button value="b" className='custom-radio custom-radio-adapter'>TARDE</Radio.Button>
+      <Radio.Button value="c" className='custom-radio custom-radio-adapter'>NOITE</Radio.Button>
     </Radio.Group>
   </Flex>
 );
