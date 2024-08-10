019cad31f62b480a3ccd18c7b7265e294292144a
unknown <db@DBS-1162.dbserver.com.br>
2024-08-09 21:50:21 -0300

    adicionando guia de rotas

--- src/components/header/headerComponent.tsx
+++ src/components/header/headerComponent.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 import { Layout, Menu, theme, Breadcrumb } from 'antd';
+import { useLocation, Link } from 'react-router-dom';
 import './style.scss';
 import logo from '../../resources/assets/logo.png'; 
+import { title } from "process";
 
 const { Header, Content } = Layout;
 
 const HeaderComponent: React.FC = () => {
+    const location = useLocation(); // Hook usado dentro do componente
+
+    // Função para gerar itens de breadcrumb com base no pathname
+    const getBreadcrumbItems = () => {
+
+    const pathnames = location.pathname.split('/').filter((x) => x);
+
+        // Define os itens baseados na rota atual
+        const breadcrumbItems = [
+            { title: <Link to="/">Inicial</Link> }
+        ];
+
+        if (pathnames.length === 0) {
+            return breadcrumbItems; // Retorna apenas "Inicial" para a página inicial
+        }
+
+        // Adiciona os itens adicionais com base na rota atual
+        if (pathnames[0] === 'atualizar') {
+            breadcrumbItems.push({ title: <Link to="/atualizar">Atualizar Dados Meteorológicos</Link> });
+        } else if (pathnames[0] === 'cadastro') {
+            breadcrumbItems.push({ title: <Link to="/cadastro">Cadastrar Dados Meteorológicos</Link> });
+        }
+        
+
+        return breadcrumbItems;
+    };
+
     return (
         <Layout>
             <Header 
@@ -32,8 +61,11 @@ const HeaderComponent: React.FC = () => {
                     padding: '40px 10%'
                 }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Inicial</Breadcrumb.Item>
-                        <Breadcrumb.Item>Cadastro de dados meteorológicos</Breadcrumb.Item>
+                        {getBreadcrumbItems().map((item, index) => (
+                            <Breadcrumb.Item key={index}>
+                                {item.title}
+                            </Breadcrumb.Item>
+                        ))}
                     </Breadcrumb>
                 </Content>
             </Layout>
